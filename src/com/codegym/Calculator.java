63ed11e2933693c7fe13562f818f146b22c3b9e6
hung060798 <hungpppp122gmail.com>
2021-06-16 16:00:33 +0700

    rafactor

---
+++ src/com/codegym/Calculator.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+
+
+public class Calculator {
+
+    public static final char ADDTION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MUTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case ADDTION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MUTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
